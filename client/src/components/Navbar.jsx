589938f6f469baec4bee67ba584a4da716bfd5d0
spiceaenj <aenjelleyace@gmail.com>
2025-03-01 21:52:14 +0800

    landingpage

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,17 +1,31 @@
-//import React from 'react'
-import { assets } from "../assets/assets"
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
+import { assets } from "../assets/assets";
 
 const Navbar = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
-    <div className="w-full flex justify-between items-center p-4 sm:p-6 sm:px-24 absolute top-0">
-      <img src={assets.montoria_home} alt="montoria logo" className="w-36 sm:w-48 hover:scale-110 transition-all" />
-      <button onClick={()=>navigate('/login')} className='flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all'>
-        Login <img src={assets.arrow_icon} alt="login button" /> </button>
-    </div>
-  )
-}
+    <nav className="fixed top-0 left-0 w-full z-50 bg-white drop-shadow-md">
+      <div className="flex justify-between items-center py-4 px-6 md:px-16 bg-white rounded-md">
+        {/* Logo at the Left */}
+        <a href="/">
+          <img
+            className="h-16 hover:scale-105 transition-all"
+            src={assets.montoria_home}
+            alt="Montoria Logo"
+          />
+        </a>
 
-export default Navbar
+        {/* Login Button at the Right */}
+        <button
+          className="text-white bg-[#9d16be] px-6 py-2 rounded-full hover:bg-[#b074c2] transition"
+          onClick={() => navigate("/login")}
+        >
+          Login
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
